b5f0b80deb1ce3f71fb3b77744cd0bc548f857f9
Natsudark13 <alejandro.r.13@hotmail.com>
2021-10-02 23:32:29 -0600

    AA

---
+++ bin/target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.poo.bieninmueble.controladores"},{"l":"com.poo.bieninmueble.dao"},{"l":"com.poo.bieninmueble.logicaDeNegocios"},{"l":"com.poo.bieninmueble.servicios"},{"l":"com.poo.bieninmueble.vistas"}]
